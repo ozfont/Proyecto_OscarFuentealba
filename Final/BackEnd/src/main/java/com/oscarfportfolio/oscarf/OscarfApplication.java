db47146d0c619874f6e1225e1b4d2c218e40367d
Oscar Fuentealba <oscar_f@outlook.com>
2022-05-26 14:58:49 -0300

    FrontEnd

---
+++ Final/BackEnd/src/main/java/com/oscarfportfolio/oscarf/OscarfApplication.java
@@ -0,0 +1,13 @@
+package com.oscarfportfolio.oscarf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OscarfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OscarfApplication.class, args);
+	}
+
+}
---
+++ Final/BackEnd/src/test/java/com/oscarfportfolio/oscarf/OscarfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscarfportfolio.oscarf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OscarfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
